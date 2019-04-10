54b664b5e184950b89813d9a7f9b4b9955618f1b
Soeren Reichardt <soren.reichardt@neotechnology.com>
2019-04-10 17:21:26 +0200

    Get rid of util.GotGraphBuilder
    - for simplicity now inlined in GoTGraphApp

--- src/main/scala/com/neo4j/algos/GoTGraphApp.scala
+++ src/main/scala/com/neo4j/algos/GoTGraphApp.scala
@@ -1,7 +1,8 @@
 package com.neo4j.algos
 
 import com.neo4j.algos.utils.GoTDataReader.{readEdgeData, readNodeData}
-import com.neo4j.algos.utils.GotGraphBuilder.createGoTGraphFrame
+import org.apache.spark.sql.DataFrame
+import org.graphframes.GraphFrame
 
 abstract class GoTGraphApp(val bookNr: Option[Int] = None) extends SparkApp {
 
@@ -9,5 +10,22 @@ abstract class GoTGraphApp(val bookNr: Option[Int] = None) extends SparkApp {
   private val edges = readEdgeData(bookNr)
 
   val g = createGoTGraphFrame(nodes, edges)
+
+  private def createGoTGraphFrame(nodesDf: DataFrame, edgeDf: DataFrame): GraphFrame = {
+    val preprocessedEdges = preprocessEdges(edgeDf)
+
+    GraphFrame(nodesDf, preprocessedEdges)
+  }
+
+  private def preprocessEdges(edgeDf: DataFrame): DataFrame = {
+    val reversedEdgesDf = edgeDf
+      .withColumn("newSrc", edgeDf.col("dst"))
+      .withColumn("newDst", edgeDf.col("src"))
+      .drop("src", "dst")
+      .withColumnRenamed("newSrc", "src")
+      .withColumnRenamed("newDst", "dst")
+
+    edgeDf.union(reversedEdgesDf)
+  }
 
 }
