b3f574bc5eb99520bafbc8624d28a0d90a8c5c4a
Soeren Reichardt <soren.reichardt@neotechnology.com>
2019-04-11 09:23:22 +0200

    Inline spark session into got graph app

--- src/main/scala/com/neo4j/algos/GoTGraphApp.scala
+++ src/main/scala/com/neo4j/algos/GoTGraphApp.scala
@@ -1,15 +1,27 @@
 package com.neo4j.algos
 
 import com.neo4j.algos.utils.GoTDataReader.{readEdgeData, readNodeData}
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.graphframes.GraphFrame
 
-abstract class GoTGraphApp(val bookNr: Option[Int] = None) extends SparkApp {
+abstract class GoTGraphApp(val bookNr: Option[Int] = None) extends App {
+
+  implicit val spark: SparkSession = {
+    val session = SparkSession
+      .builder()
+      .config(new SparkConf(true))
+      .appName("Spark AI Demo")
+      .master("local[*]")
+      .getOrCreate()
+    session.sparkContext.setLogLevel("error")
+    session
+  }
+
+  val g = createGoTGraphFrame(nodes, edges)
 
   private val nodes = readNodeData(bookNr)
   private val edges = readEdgeData(bookNr)
-
-  val g = createGoTGraphFrame(nodes, edges)
 
   private def createGoTGraphFrame(nodesDf: DataFrame, edgeDf: DataFrame): GraphFrame = {
     val preprocessedEdges = preprocessEdges(edgeDf)
