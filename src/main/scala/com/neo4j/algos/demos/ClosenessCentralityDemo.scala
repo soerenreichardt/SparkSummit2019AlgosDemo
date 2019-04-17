8d380064359d59ae6b696ea57624239fb780c562
Soeren Reichardt <soren.reichardt@neotechnology.com>
2019-04-17 20:57:36 +0200

    Implement closeness centrality and add tv show dataset
    - closeness centrality currently crashes

--- src/main/scala/com/neo4j/algos/demos/ClosenessCentralityDemo.scala
+++ src/main/scala/com/neo4j/algos/demos/ClosenessCentralityDemo.scala
@@ -12,9 +12,9 @@ object ClosenessCentralityDemo extends GoTGraphApp {
 
   import spark.implicits._
 
-  val vertices = g.vertices.withColumn("ids", struct(array(), array()))
+  val vertices = g.vertices.withColumn("ids", array(struct($"id", lit(1).as("distance"))))
   val cachedVertices = AggregateMessages.getCachedDataFrame(vertices)
-  val g2 = GraphFrame(cachedVertices, g.edges)
+  var g2 = GraphFrame(cachedVertices, g.edges)
 
   (0l to g2.vertices.count).foreach { _ =>
     val msgDst = newPathsUdf.apply(AggregateMessages.src("ids"), AggregateMessages.src("id"))
@@ -24,32 +24,51 @@ object ClosenessCentralityDemo extends GoTGraphApp {
       .sendToDst(msgDst)
       .agg(collect_set(AggregateMessages.msg).alias("agg"))
     val res = agg.withColumn("newIds", flattenUdf($"agg")).drop($"agg")
-    res.show
-//    val newVertices = g2.vertices.join(res, $"id", "left_outer")
-//      .withColumn("mergedIds", mergePathsUdf($"ids", $"newIds"))
-//    newVertices.show
+    val newVertices = g2.vertices.join(res, Seq("id"), "left_outer")
+      .withColumn("mergedIds", mergePathsUdf($"ids", $"newIds", $"id"))
+      .drop("ids", "newIds")
+      .withColumnRenamed("mergedIds", "ids")
+    val cachedNewVertices = AggregateMessages.getCachedDataFrame(newVertices)
+    g2 = GraphFrame(cachedNewVertices, g2.edges)
   }
 
-  def pathsType = ArrayType(
+  g2.vertices
+    .withColumn("closeness", closenessUdf($"ids"))
+    .sort($"closeness".desc)
+    .show(false)
+
+  case class PathsType(id: String, distance: Int)
+  val pathsType = ArrayType(
     StructType(Seq(
-      StructField("id", StringType),
-      StructField("distance", IntegerType)
-    ))
+      StructField("id", StringType, true),
+      StructField("distance", IntegerType, true))
+    )
   )
 
-  def newPathsUdf: UserDefinedFunction = udf((paths: Row, id: String) => {
-    paths.getAs[Seq[String]](0).zip(paths.getAs[Seq[Int]](1)).foldLeft(Seq((id, 1))) {
-      case (acc, (pathId, distance)) if pathId != id => acc :+ (pathId, distance + 1)
-      case (acc, (pathId, distance)) => acc :+ (pathId, distance)
+  def newPathsUdf: UserDefinedFunction = udf[Seq[PathsType], Seq[Row], String]((paths: Seq[Row], id: String) => {
+    paths.foldLeft(Seq.empty[PathsType]) {
+      case (acc, pType) if pType.getAs[String](0) != id => acc :+ PathsType(pType.getAs[String](0), pType.getAs[Int](1) + 1)
+      case (acc, pType) => acc :+ PathsType(pType.getAs[String](0), pType.getAs[Int](1))
     }
   })
 
   def flattenUdf: UserDefinedFunction = udf((ids: Seq[Seq[Row]]) => {
-    ids.flatten.map(_.getAs[String](0))
+    ids.flatten.map(row => PathsType(row.getAs[String](0), row.getAs[Int](1)))
   })
 
-  def mergePathsUdf: UserDefinedFunction = udf(() => {
+  def mergePathsUdf: UserDefinedFunction = udf[Seq[PathsType], Seq[Row], Seq[Row], String]((ids: Seq[Row], newIds: Seq[Row], id: String) => {
+    (ids ++ newIds)
+      .map(row => PathsType(row.getAs[String](0), row.getAs[Int](1)))
+      .filter(_.id == id)
+      .sortBy(_.distance)
+  })
 
+  def closenessUdf: UserDefinedFunction = udf[Double, Seq[Row]]((ids: Seq[Row]) => {
+    val nodes = ids.length
+    val totalDistance = ids.map(_.getAs[Int](1)).sum
+
+    if (totalDistance == 0) 0
+    else nodes * 1.0 / totalDistance
   })
 }
 
